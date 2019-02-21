dcab57161cdad57e80049af3c9292f9d192a5cb1
fullstackmommy <nicole.budiman@gmail.com>
2019-02-21 16:53:15 +0800

    Solution Part 9a: Change Counter component

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,38 +1,32 @@
-import React, {Component} from 'react'
+import React from 'react'
 
-export class Counter extends Component {
-
-    evaluateClass = () => {
-        return this.props.value > 0
+export default function Counter(props) {
+    const evaluateClass = () => {
+        return props.value > 0
             ? "primary"
             : "warning"
-
     }
 
-    render() {
-        return (
-            <React.Fragment>
-                <h2>
-                    <span className={`badge badge-${this.evaluateClass()} m-2`}>{this.props.value}</span>
-                </h2>
-                <button
-                    type="button"
-                    className="btn btn-primary m-2"
-                    onClick={() => this.props.handleClickUp(this.props.id)}>+</button>
-                <button
-                    type="button"
-                    className="btn btn-warning m-2"
-                    onClick={() => this.props.handleClickDown(this.props.id)}>-</button>
-                <button
-                    type="button"
-                    className="btn btn-secondary m-2"
-                    onClick={() => this.props.handleReset(this.props.id)}>Reset</button>
-                <button
-                    className="btn btn-secondary"
-                    onClick={() => this.props.handleDelete(this.props.id)}>Delete</button>
-            </React.Fragment>
-        )
-    };
+    return (
+        <React.Fragment>
+            <h2>
+                <span className={`badge badge-${evaluateClass()} m-2`}>{props.value}</span>
+            </h2>
+            <button
+                type="button"
+                className="btn btn-primary m-2"
+                onClick={() => props.handleClickUp(props.id)}>+</button>
+            <button
+                type="button"
+                className="btn btn-warning m-2"
+                onClick={() => props.handleClickDown(props.id)}>-</button>
+            <button
+                type="button"
+                className="btn btn-secondary m-2"
+                onClick={() => props.handleReset(props.id)}>Reset</button>
+            <button
+                className="btn btn-secondary"
+                onClick={() => props.handleDelete(props.id)}>Delete</button>
+        </React.Fragment>
+    )
 }
-
-export default Counter
