704054cdf6d6794478346690a4c336759678f058
fullstackmommy <nicole.budiman@gmail.com>
2019-02-21 16:11:50 +0800

    Solution Part 8: Add Reset All button

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,58 +1,35 @@
 import React, {Component} from 'react'
 
 export class Counter extends Component {
-    state = {
-        id: this.props.id,
-        value: this.props.value
-    };
-
-    // use arrow function when creating event handler, to avoid the need to bind
-    handleClickUp = () => {
-        const val = this.state.value + 1;
-        this.setState({value: val});
-        // use setState to inform the system that there is a change. Also, avoid
-        // changing original value
-    }
-
-    handleClickDown = () => {
-        const val = this.state.value - 1;
-        if (this.state.value !== 0) {
-            this.setState({value: val});
-        } else {
-            console.log("Counter is 0");
-        }
-    }
 
     evaluateClass = () => {
-        return this.state.value > 0
+        return this.props.value > 0
             ? "primary"
             : "warning"
 
-    }
-
-    handleReset = () => {
-        this.setState({value: 0});
     }
 
     render() {
         return (
             <React.Fragment>
                 <h2>
-                    <span className={`badge badge-${this.evaluateClass()} m-2`}>{this.state.value}</span>
+                    <span className={`badge badge-${this.evaluateClass()} m-2`}>{this.props.value}</span>
                 </h2>
                 <button
                     type="button"
                     className="btn btn-primary m-2"
-                    onClick={this.handleClickUp}>+</button>
+                    onClick={() => this.props.handleClickUp(this.props.id)}>+</button>
                 <button
                     type="button"
                     className="btn btn-warning m-2"
-                    onClick={this.handleClickDown}>-</button>
-                <button type="button" className="btn btn-info m-2" onClick={this.handleReset}>Reset
-                </button>
+                    onClick={() => this.props.handleClickDown(this.props.id)}>-</button>
+                <button
+                    type="button"
+                    className="btn btn-secondary m-2"
+                    onClick={() => this.props.handleReset(this.props.id)}>Reset</button>
                 <button
                     className="btn btn-secondary"
-                    onClick={() => this.props.handleDelete(this.state.id)}>Delete</button>
+                    onClick={() => this.props.handleDelete(this.props.id)}>Delete</button>
             </React.Fragment>
         )
     };
--- src/components/Counters/Counters.jsx
+++ src/components/Counters/Counters.jsx
@@ -7,6 +7,22 @@ export class Counters extends Component {
         data: counters
     };
 
+    settingState = (counterId, operator) => {
+        const copy = [...this.state.data];
+        copy
+            .find(element => element.id === counterId)
+            .value += operator;
+        this.setState({data: copy});
+    }
+
+    handleClickUp = (counterId) => {
+        this.settingState(counterId, 1)
+    }
+
+    handleClickDown = (counterId) => {
+        this.settingState(counterId, -1)
+    }
+
     /* created in the parent, instead of child: if the button is in the child component but the states need to be maintained in the parent*/
     handleDelete = (counterId) => {
         const filteredData = this
@@ -16,17 +32,35 @@ export class Counters extends Component {
         this.setState({data: filteredData});
     }
 
+    handleResetAll = () => {
+        const resetData = this.state.data;
+        resetData.map(counter => counter.value = 0);
+        this.setState({data: resetData});
+    }
+
+    handleReset = (counterId) => {
+        const copy = [...this.state.data];
+        copy
+            .find(element => element.id === counterId)
+            .value = 0;
+        this.setState({data: copy});
+    }
+
     render() {
         const {data} = this.state;
 
         return (
             <div>
+                <button className="btn btn-danger" onClick={this.handleResetAll}>Reset All</button>
                 {data.length === 0 && <h2>No more counters</h2>}
                 {data.map(counter => (<Counter
                     key={counter.id}
                     id={counter.id}
                     value={counter.value}
-                    handleDelete={this.handleDelete}/>))
+                    handleDelete={this.handleDelete}
+                    handleClickUp={this.handleClickUp}
+                    handleClickDown={this.handleClickDown}
+                    handleReset={this.handleReset}/>))
 }
             </div>
         )
